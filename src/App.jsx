efc2921edb313e4a9a8c225fcb3dc5cdeffcedfd
Jash <jashsharma619@gmail.com>
2023-08-27 22:38:52 +0530

    default scoll bar changed

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,15 @@ import "./App.css";
 import Navbar from "./components/Navbar";
 import Home from "./components/Home";
 import Bio from "./components/Bio";
+import Footer from "./components/Footer";
 function App() {
   return (
     <div className="App">
       <div></div>
       <Navbar />
       <Home />
-      <Bio/>
+      <Bio />
+      <Footer />
     </div>
   );
 }
