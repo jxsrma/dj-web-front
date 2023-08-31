5e87e8ad9306e3b549448138fc3aa8355891f917
Jash <jashsharma619@gmail.com>
2023-08-31 10:45:07 +0530

    Design changes

--- src/components/Youtube.jsx
+++ src/components/Youtube.jsx
@@ -4,20 +4,36 @@ import "../css/youtube.css";
 function Youtube() {
   return (
     <div className="youtube-main">
-      <p>
-        <i class="fa-brands fa-youtube"></i>
-        &nbsp; Youtube
-      </p>
-      <div className="youtube-main-gallery">
-        <iframe
-          width="1280"
-          height="533"
-          src="https://www.youtube.com/embed/fPWfTaDFaLg"
-          title="JXSRMA - Athena"
-          frameborder="0"
-          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-          allowfullscreen
-        ></iframe>
+      <div className="youtube-heading">
+        Listen my Latest release on&nbsp;
+        <img
+          src="https://freelogopng.com/images/all_img/1656504144youtube-logo-png-white.png"
+          alt=""
+        />
+      </div>
+      <div className="youtube-content">
+        <div className="youtube-main-gallery">
+          <iframe
+            src="https://www.youtube.com/embed/fPWfTaDFaLg"
+            title="JXSRMA - Athena"
+            frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            allowfullscreen
+          ></iframe>
+        </div>
+        <div className="youtube-para">
+          <p>
+            Welcome to the official YouTube channel of JXSRMA! 🔥 Dive into a
+            world of electrifying beats, futuristic melodies, and captivating
+            sounds. 🎶 JXSRMA is a talented music producer and artist, pushing
+            the boundaries of electronic music genres.
+          </p>
+          <p>
+            Subscribe now and hit the notification bell 🔔 to never miss a beat!
+            Get ready to immerse yourself in the sonic world of JXSRMA and
+            experience the power of electronic music like never before. 💥
+          </p>
+        </div>
       </div>
     </div>
   );
