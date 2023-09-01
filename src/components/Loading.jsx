7cbbb20882b2e1fb44962ff416611e9b01abb951
Jash <jashsharma619@gmail.com>
2023-09-01 16:25:37 +0530

    new loading screen added

---
+++ src/components/Loading.jsx
@@ -0,0 +1,25 @@
+import React, { useState, useEffect } from "react";
+import "../css/loadingScreen.css";
+
+function Loading() {
+  // Create a state variable to control when to hide the loading screen
+  const [isLoading, setIsLoading] = useState(true);
+
+  // Simulate loading by setting a timeout
+  useEffect(() => {
+    const fakeLoading = setTimeout(() => {
+      setIsLoading(false);
+    }, 3000); // Adjust the time (in milliseconds) as needed
+    return () => clearTimeout(fakeLoading);
+  }, []);
+
+  return (
+    <div>
+      <div className={`loading-screen ${isLoading ? "loading-show" : ""}`}>
+        <div className="loader">loading</div>
+      </div>
+    </div>
+  );
+}
+
+export default Loading;
